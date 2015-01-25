64dc0d6e8abddea3274f8315e030f4af5dbf5517
Elva Tristan <elva@mblz.com>
2015-01-25 12:39:55 -0600

    updates for fusion and click_to_call

--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 require "lib/site_helper"
 helpers SiteHelper
 
+# Mucked with assets generally
+#set :http_prefix, "//assets.integrated-internet.com/sites/#{$SITE ? $SITE : 'default'}/"
+
 activate :directory_indexes
 
 # Use relative URLs
@@ -13,10 +16,6 @@ set :js_dir, 'js'
 set :images_dir, 'img'
 
 
-activate :deploy do |deploy|
-  deploy.method = :git
-end
-
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
@@ -25,6 +24,10 @@ end
 
 # Build-specific configuration
 configure :build do
+  # Only include this sites' content
+  ignore /^content\/(?!#{$SITE})/
+  ignore /^\.git/
+
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
@@ -41,3 +44,17 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
+activate :deploy do |deploy|
+  deploy.build_before = true # default: false
+  deploy.method       = :rsync
+  deploy.host         = '104.236.159.21'
+  # deploy.port       = 5555
+  deploy.path         = "~/static/sites/#{$SITE}"
+  deploy.user         = 'mblz' # no default
+  #deploy.flags  = '--exclude assets'
+  #   # Optional Settings
+  #   # deploy.user  = 'tvaughan' # no default
+  #   # deploy.port  = 5309 # ssh port, default: 22
+  #   # deploy.clean = true # remove orphaned files on remote host, default: false
+  #   # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
+end
--- lib/site_helper.rb
+++ lib/site_helper.rb
@@ -1,3 +1,49 @@
+$SITE = ENV["site"] || 'site'
+p "$SITE=#{$SITE}"
+
+# if $SITE
+#   p "Booting up $SITE=#{$SITE}"
+# else
+#   p "$SITE NOT FOUND - using default"
+# end
+
+
 module SiteHelper
 
+	def site
+  	site_data = eval("data.sites.#{$SITE}")
+
+    rescue
+      throw("Could not find default site data in data/sites/#{$SITE}")
+	end	
+
+	def site_partial part, *args
+		partial("partials/sites/#{$SITE}/#{part}", *args)
+	rescue
+		partial("partials/sites/site/#{part}", *args)
+	end
+
+	def site_img img, *args
+		image_tag(site_img_path(img), *args)
+	end
+	def site_img_path img, *args
+	  "//assets.integrated-internet.com/sites/#{$SITE}/img/#{img}" 
+	end	
+
+	def click_to_call
+		site_partial('click_to_call')
+	end
+
+	def titleize title
+    ActiveSupport::Inflector.titleize(title)
+  end
+end
+
+# So we can use setting.name rather than setting[:name]
+class ::Hash
+  def method_missing(name)
+    return self[name] if key? name
+    self.each { |k,v| return v if k.to_s.to_sym == name }
+    super.method_missing name
+  end
 end
