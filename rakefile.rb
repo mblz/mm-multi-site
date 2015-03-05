74822d69cfc5a25681e6fc28b8258296dfc17241
Elva Tristan <elva@mblz.com>
2015-03-05 12:03:34 -0600

    mblz site

--- rakefile.rb
+++ rakefile.rb
@@ -38,3 +38,24 @@ task :build_all do
     system "site=#{name} middleman build"
   end
 end
+
+desc "create thumbs for this dir"
+task :thumbs, [:site, :dir] do |t, args|
+  site = args[:site]
+  dir  = args[:dir]
+  dir_path = File.expand_path("~/Pictures/assets/sites/#{site}/assets/img/#{dir}")
+  system 'mkdir', '-p', dir_path + '/thumb'
+  imgs     = Dir.glob(dir_path + "/*[jpg|png|gif]")
+  imgs.each do |img_path|
+    parts = img_path.split('/')
+    name  = parts.delete(parts.last)
+    thumb = [parts,"thumb", name].join('/')
+    if File.exists?(thumb)
+      #p "Exists: #{thumb}"
+    else
+      p "Thumbnail: #{thumb}"
+      format = name.split('.').last
+      system "mogrify -resize x100 -background white -gravity center -extent x100 -format #{format} -quality 80 -path '#{parts.join('/')}/thumb' '#{img_path}'"
+    end
+  end
+end
