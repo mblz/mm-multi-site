743b275d63e3f91037d6f4a842232ba6fab1549d
Paul Vudmaska <paul@vudmaska.com>
2015-04-23 16:09:20 -0500

    site mag

--- rakefile.rb
+++ rakefile.rb
@@ -26,7 +26,7 @@ end
 desc "just show sites"
 task :mm, [:site] do |t, args|
   site = args[:site] || ask_for_site()[:name]
-  system "subl ./data/sites/#{site}/site.yml ./source/content/#{site}"
+  system "/usr/local/bin/sublime ./data/sites/#{site}/site.yml ./source/content/#{site}"
   system "open http://localhost:4567/ && site=#{site} bundle exec middleman"
 
 end
@@ -35,7 +35,7 @@ end
 desc "open site in sublime"
 task :edit, [:site] do |t, args|
   site = args[:site] || ask_for_site()[:name]
-  system "subl ./data/sites/#{site}/site.yml ./source/content/#{site}"
+  system "/usr/local/bin/sublime ./data/sites/#{site}/site.yml ./source/content/#{site}"
 end
 
 
